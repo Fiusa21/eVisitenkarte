9cd58f232d489626abf05575f12dbab67d9fae26
TobiasWahl1 <wahl663@gmail.com>
2025-11-14 17:34:32 +0100

    feat: created logout component

---
+++ frontend/src/components/Logout.vue
@@ -0,0 +1,22 @@
+<template>
+    <button @click="logout" class="logout-button">Logout</button>
+</template>
+
+<script>
+export default {
+    name: 'logout',
+    setup() {
+
+        //Adjust to keycloak-services
+        const logout = () => {
+            keycloakServices.logout();
+            isAuthenticated = false;
+            backendResponse = null;
+            error = null;
+        };
+        return {
+        logout,
+        };
+    }
+}
+</script>
