1f9b706a47d84d437a5b21eed2b2709974b06ecd
TobiasWahl1 <wahl663@gmail.com>
2025-11-15 13:08:56 +0100

    style: styled LoginLanding button and added Logo

--- frontend/src/views/LoginLanding.vue
+++ frontend/src/views/LoginLanding.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="login-container">
-    <h1>Anmeldung erforderlich</h1>
-    <p>Bitte klicken Sie auf "Anmelden", um sich mit Keycloak zu authentifizieren und zur Anwendung zu gelangen.</p>
-    <button @click="handleLogin">Zur Anmeldung</button>
-  </div>
+    <div class="site-header">
+      <div class="header-content">
+        <h1>uxitra GmbH</h1>
+        <p>eVisitenkarten Editor</p>
+      </div>
+    </div>
+    <div class="login-container">
+        <h1>Anmeldung erforderlich</h1>
+        <p>Bitte klicken Sie auf "Anmelden", um sich mit Keycloak zu authentifizieren und zur Anwendung zu gelangen.</p>
+        <button @click="handleLogin">Anmelden</button>
+    </div>
 </template>
 
 <script setup>
@@ -36,3 +42,61 @@ onMounted(async () => {
     }
 });
 </script>
+
+<style scoped>
+.login-container h1{
+    font-size: 3em;
+    font-family: 'Dosis', sans-serif;
+    font-weight: 500;
+    margin: 0;
+    padding: 0;
+    line-height: 1;
+}
+
+.header-content {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+}
+
+.site-header h1{
+    font-size: 5em;
+    font-family: 'Dosis', sans-serif;
+    font-weight: 500;
+    margin: 0;
+    padding: 0;
+    line-height: 1;  
+}
+
+.site-header p{
+    font-size: 1.5em;
+    font-family: 'Dosis', sans-serif;
+    font-weight: 300;
+    margin: 0;
+    padding: 0;
+    margin-top: -5px;
+}
+
+.login-container button{
+    min-width: 120px;
+    height: 36px;
+    padding: 6px 14px;
+    border-radius: 18px;
+    background: transparent;
+    color: white;
+    border: none;
+    cursor: pointer;
+    font-size: 1.2em;
+    font-family: 'Dosis', sans-serif;
+    background: none;
+    border: none;
+    color: white;
+    padding: 5px 10px;
+    cursor: pointer;
+    outline: none;
+}
+
+button:hover{
+    background-color: #FB8500;
+}
+</style>
