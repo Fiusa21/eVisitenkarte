de2770e65d5fc7a83d292eeec5dc0013c491fb89
TobiasWahl1 <wahl663@gmail.com>
2025-11-21 13:26:15 +0100

    restructure

--- frontend/src/views/AdminDashboard.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <h2>Admin Dashboard</h2>
-  <p>Overview of system activity and statistics goes here.</p>
-</template>
-<script>export default { name: 'AdminDashboard' }</script>
-<style scoped></style>
--- frontend/src/views/Nutzerverwaltung.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <h2>Nutzerverwaltung</h2>
-  <p>This page will contain the searchable, sortable table of all users.</p>
-  </template>
-<script>export default { name: 'Nutzerverwaltung' }</script>
-<style scoped></style>
